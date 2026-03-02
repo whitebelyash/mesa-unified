d8e4b8294b880acf147f045c337c57cb58264b5e
whitebelyash <whbexiumwork@gmail.com>
2026-03-02 13:34:05 +0400

    freedreno/shim: enable drm-shim support for Adreno 810/825/829
    Cache sizes for 810&825 should be valid. 829 is unknown, but works so
    far(?)
    
    Signed-off-by: whitebelyash <whbexiumwork@gmail.com>

--- src/freedreno/drm-shim/freedreno_noop.c
+++ src/freedreno/drm-shim/freedreno_noop.c
@@ -339,6 +339,21 @@ static const struct msm_device_info device_infos[] = {
       .chip_id = 0x44050000,
       .gmem_size = 12 * 1024 * 1024,
    },
+   {
+      .gpu_id = 825,
+      .chip_id = 0x44030000,
+      .gmem_size = 2 * 1024 * 1024,
+   },
+   {
+      .gpu_id = 829,
+      .chip_id = 0x44030A20,
+      .gmem_size = 2 * 1024 * 1024,
+   },
+   {
+      .gpu_id = 810,
+      .chip_id = 0x44010000,
+      .gmem_size = 576 * 1024,
+   }
 };
 
 static void
