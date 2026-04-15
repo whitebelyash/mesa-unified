031918eca47524cb6ef4b3e9365ce6ff8d9a9333
DVD <dmitrijnemov13@gmail.com>
2026-04-15 23:44:38 +0300

    freedreno/common: update a8xx family configs
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>
    
    Add bool gmem_size
    
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>
    
    Fixing a compilation error
    
    Moved gmem_size to props structure in freedreno_dev_info.h
    
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>
    
    Adjust tile alignment widths and heights in freedreno_devices
    
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>
    
    Update GPUProps for a8xx with new features
    
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>
    
    Update gmem_per_ccu_depth_cache_size value for A829
    
    Changed gmem_per_ccu_depth_cache_size from 192KB to 128KB.
    
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>
    
    Update freedreno_devices.py
    
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>
    
    Change gmem_per_ccu_depth_cache_size to 128KB
    
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>
    
    Update GPU memory cache settings in freedreno_devices.py
    
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>
    
    Change color and depth cache fractions to HALF
    
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>
    
    Adjust sysmem per CCU depth cache size
    
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>
    
    Adjust memory cache sizes and tile alignment
    
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>
    
    Change tile width alignment from 64 to 96
    
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>
    
    Adjust cache fraction and tile alignment values
    
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>

--- src/freedreno/common/freedreno_dev_info.h
+++ src/freedreno/common/freedreno_dev_info.h
@@ -512,6 +512,9 @@ struct fd_dev_info {
       bool subpass_fence_cleans_resolve;
       /* If GMEM needs to be disabled for this GPU */
       bool disable_gmem;
+
+      /* GMEM size in bytes */
+      uint32_t gmem_size;
    } props;
 };
 
