17d99cd451a993514b371e27b8b7e10653a7858b
whitebelyash <whbexiumwork@gmail.com>
2026-05-10 19:24:51 +0400

    zink: support loading vulkan from pojavexec-provided handle

--- src/gallium/drivers/zink/zink_screen.c
+++ src/gallium/drivers/zink/zink_screen.c
@@ -3399,8 +3399,12 @@ zink_internal_create_screen(const struct pipe_screen_config *config, int64_t dev
 
 
    u_trace_state_init();
-
-   screen->loader_lib = util_dl_open(VK_LIBNAME);
+   
+   screen->loader_lib = dlopen("libmjlvlk.so", RTLD_NOLOAD | RTLD_NOW);
+   if(!screen->loader_lib) {
+      mesa_loge("ZINK: failed to dlopen existing vulkan lib, falling back to system! %s", dlerror());
+      screen->loader_lib = util_dl_open(VK_LIBNAME);
+   }
    if (!screen->loader_lib) {
       if (!screen->driver_name_is_inferred)
          mesa_loge("ZINK: failed to load "VK_LIBNAME);
