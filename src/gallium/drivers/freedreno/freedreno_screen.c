63aa5c700cfdc8daf391bfd362ce29b44230758b
whitebelyash <whbexiumwork@gmail.com>
2026-05-12 14:26:41 +0400

    freedreno: add MojoLauncher to the vendor string

--- src/gallium/drivers/freedreno/freedreno_screen.c
+++ src/gallium/drivers/freedreno/freedreno_screen.c
@@ -100,7 +100,7 @@ fd_screen_get_name(struct pipe_screen *pscreen)
 static const char *
 fd_screen_get_vendor(struct pipe_screen *pscreen)
 {
-   return "freedreno";
+   return "freedreno/MojoLauncher";
 }
 
 static const char *
