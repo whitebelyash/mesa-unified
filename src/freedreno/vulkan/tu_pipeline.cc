26e6077f30b16cca9b34922fccb995e7f4063000
DVD <disk_dvd@outlook.com>
2026-06-02 07:33:48 +0300

    Freedreno/vulcan/tu_pipeline.cc: On custom resolve for A810, off MSAA, FDM for A810,825,829,830
    As practice has shown, this gives an increase in dx12 games, but does not provide any increase in dx11 In games, artifacts don't appear. This applies to the A810-830, I think because they have the same blob. That is, there's no increase on the A840.

--- src/freedreno/vulkan/tu_pipeline.cc
+++ src/freedreno/vulkan/tu_pipeline.cc
@@ -1743,13 +1743,12 @@ tu_pipeline_builder_compile_shaders(struct tu_pipeline_builder *builder,
    };
    VkPipelineCreationFeedback stage_feedbacks[MESA_SHADER_STAGES] = { 0 };
 
-   /* === ДОБАВЛЕНО: Идентификация GPU Adreno 8xx === */
    const uint64_t chip_id = builder->device->physical_device->dev_id.chip_id;
    const bool is_a810 = chip_id == 0x44010000ull;
    const bool is_a825 = chip_id == 0x44030000ull;
    const bool is_a829 = chip_id == 0x44030A20ull;
-   const bool is_target_gpu = is_a810 || is_a825 || is_a829;
-   /* === КОНЕЦ ДОБАВЛЕНИЯ === */
+   const bool is_a830 = chip_id == 0xffff44050000 || 0x44050001;
+   const bool is_target_gpu = is_a810 || is_a825 || is_a829 || is_a830;
 
    const bool executable_info =
       builder->create_flags &
@@ -1854,9 +1853,8 @@ tu_pipeline_builder_compile_shaders(struct tu_pipeline_builder *builder,
 
    if (builder->state &
        VK_GRAPHICS_PIPELINE_LIBRARY_FRAGMENT_SHADER_BIT_EXT) {
-      /* === ИЗМЕНЕНО: Отключаем custom_resolve на A810 === */
       keys[MESA_SHADER_FRAGMENT].custom_resolve =
-         is_a810 ? false : builder->graphics_state.rp->custom_resolve;
+         builder->graphics_state.rp->custom_resolve;
 
       if (builder->device->physical_device->instance->emulate_alpha_to_coverage) {
          keys[MESA_SHADER_FRAGMENT].emulate_alpha_to_coverage = true;
@@ -1909,9 +1907,8 @@ tu_pipeline_builder_compile_shaders(struct tu_pipeline_builder *builder,
          }
       }
 
-      /* === ИЗМЕНЕНО: Отключаем FDM per layer на A810 === */
       keys[last_pre_rast_stage].fdm_per_layer =
-         is_a810 ? false : builder->fdm_per_layer;
+         is_target_gpu ? false : builder->fdm_per_layer;
    }
 
    if (builder->state & VK_GRAPHICS_PIPELINE_LIBRARY_FRAGMENT_SHADER_BIT_EXT) {
@@ -1952,9 +1949,8 @@ tu_pipeline_builder_compile_shaders(struct tu_pipeline_builder *builder,
        * just checked in tu6_emit_fs_inputs.  We will also copy the value to
        * tu_shader_key::force_sample_interp in a bit.
        */
-      /* === ИЗМЕНЕНО: Отключаем force_sample_interp на A810 === */
       keys[MESA_SHADER_FRAGMENT].force_sample_interp =
-         is_a810 ? false : (!builder->rasterizer_discard && msaa_info && msaa_info->sampleShadingEnable);
+         is_target_gpu ? false : (!builder->rasterizer_discard && msaa_info && msaa_info->sampleShadingEnable);
    }
 
    unsigned char pipeline_blake3[BLAKE3_KEY_LEN];
