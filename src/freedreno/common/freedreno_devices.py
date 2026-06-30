6bb90500022106ad1c3a3b96e45cf62db47f361e
Anmol Singh <162159622+Anmol6002@users.noreply.github.com>
2026-06-30 10:11:58 +0530

    Update freedreno_devices.py Snapdragon 4 gen 2 a613 unofficial support compiled for termux mesa it worked while forcing vulkan 1.4 specialised build and just 1.3 force from ifdevs source now testing for emulator normal version no forcing anything because this source does that already i guess I saw some commit

--- src/freedreno/common/freedreno_devices.py
+++ src/freedreno/common/freedreno_devices.py
@@ -269,6 +269,7 @@ add_gpus([
         GPUId(608), # TODO: Test it, based only on libwrapfake dumps
         GPUId(610),
         GPUId(612), # TODO: Test it, based only on libwrapfake dumps
+        GPUId(613), # Snapdragon 4 gen 2 unofficial support 
     ], A6xxGPUInfo(
         CHIP.A6XX,
         [a6xx_base, a6xx_gen1_low],
