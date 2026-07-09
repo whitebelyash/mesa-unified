f9e4f716e05a87f9bdcccc350b3b0950def68672
Rob Clark <rob.clark@oss.qualcomm.com>
2026-07-09 10:20:18 -0700

    freedreno/a6xx: Early return if no flushes to add
    Signed-off-by: Rob Clark <rob.clark@oss.qualcomm.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42998>

--- src/gallium/drivers/freedreno/a6xx/fd6_barrier.cc
+++ src/gallium/drivers/freedreno/a6xx/fd6_barrier.cc
@@ -75,6 +75,9 @@ add_flushes(struct pipe_context *pctx, unsigned flushes)
 
    DBG("flushes=0x%x", flushes);
 
+   if (!flushes)
+      return;
+
    /* If there is an active compute/nondraw batch, that is the one
     * we want to add the flushes to.  Ie. last op was a launch_grid,
     * if the next one is a launch_grid then the barriers should come
